59418ffa85308d261763037d8b3bdc265b0189cb
alketh <Alexander.Keth@uni-hamburg.de>
2017-10-18 16:20:30 +0200

    Edit plot2.

--- plot2.R
+++ plot2.R
@@ -4,6 +4,8 @@ dd <- read_clean()
 
 ylab <- "Global Active Power"
 
+dd$dt <- lubridate::ymd_hms(paste(dd$date, dd$time))
+
 png(filename = "plot2.png")
-plot(dd$date, dd$gap, ylab = paste(ylab, "(kilowatts)"))
+plot(dd$dt, dd$gap, ylab = paste(ylab, "(kilowatts)"), type = "l", xlab = "")
 dev.off()
